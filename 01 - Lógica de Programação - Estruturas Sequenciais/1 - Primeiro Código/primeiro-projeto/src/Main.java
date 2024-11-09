840e748bced3d11dd48f4d1095e81ffe2a683735
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-09 18:47:17 -0300

    1 - Primeiro Código

---
+++ 01 - Lógica de Programação - Estruturas Sequenciais/1 - Primeiro Código/primeiro-projeto/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    /**
+     * <h1> Treinamento Minha Vaga Dev Java - Lógica de Programação: Estruturas Sequencias </h1>
+     * 1 - Primeiro Código<br><br>
+     * <p>
+     * <b>Note:</b> Desenvolvido na linguagem Java.
+     *
+     * @author  Marcos Ferreira Shirafuchi
+     * @version 1.0
+     * @since   09/11/2024
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Bem vindos ao curso Minha Vaga Dev Java!");
+    }
+}
