312d7f8b73f5a7ed9b339f989168241a613397cb
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-27 20:38:37 -0300

    3 - Tipos Primitivos Em Java

---
+++ 01 - Lógica de Programação - Estruturas Sequenciais/1 - Primeiro Código/primeiro-projeto/src/tiposprimitivosemjava/TiposPrimitivosEmJava.java
@@ -0,0 +1,26 @@
+package tiposprimitivosemjava;
+
+public class TiposPrimitivosEmJava {
+    /**
+     * <h1> Treinamento Minha Vaga Dev Java - Lógica de Programação: Estruturas Sequencias </h1>
+     * 3 - Tipos Primitivos Em Java<br><br>
+     * <p>
+     * <b>Note:</b> Desenvolvido na linguagem Java.
+     *
+     * @author  Marcos Ferreira Shirafuchi
+     * @version 1.0
+     * @since   27/11/2024
+     */
+    public static void main(String[] args) {
+        int idade = 35;
+        long quantidadeCachorros = 10L;
+        double salarioMensal = 4500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Empregado: " + empregadoJava + " Idade: " + idade);
+    }
+}
